7fc0cb26d590a36b10c05937560b4f61997627e1
mohammedelhaj98-star <mohammedelhaj98@gmail.com>
2026-03-16 23:47:45 +0300

    Start Hostinger in single-threaded mode

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Hostinger shared hosting can fail hard when Node tries to create its default
 // background worker pools. Keep the runtime footprint deliberately small.
-process.env.UV_THREADPOOL_SIZE ??= "2";
+process.env.UV_THREADPOOL_SIZE ??= "1";
 
 require("./.next/standalone/server.js");
